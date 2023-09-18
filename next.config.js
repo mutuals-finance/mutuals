1a4571386bf3a76c33d0f0a9fde473240ce0f743
Fabian Piper <fabianpiper@web.de>
2023-09-18 11:29:59 +0200

    fix: ipfs cloudflare gateway url

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ module.exports = {
       'polygonscan.com',
       'afksystem.finance',
       'ipfs.io',
+      'cf-ipfs.com',
       'oropocket.com',
       'matic.ripae.finance',
       'bookie.farm',
