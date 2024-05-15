81fcac3ef51d0ed14d9ede43330148328d4d4624
Fabian Piper <fabianpiper@web.de>
2024-05-15 11:02:59 +0200

    build: custom component generator

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,14 @@
+// Unmodified components exported directly from ChakraUI
+export * from "@chakra-ui/react";
+
+/**
+ * Custom Re-exports
+ *
+ */
+
+// TODO
+
+/**
+ * Custom Components
+ * These components are custom to @splitfi/ui and are not included in ChakraUI, although they may rely on ChakraUI components.
+ */
--- packages/ui/turbo/generators/config.ts
+++ packages/ui/turbo/generators/config.ts
@@ -1,30 +1,32 @@
-import type { PlopTypes } from "@turbo/gen";
-
-// Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
+import type {PlopTypes} from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
-  plop.setGenerator("react-component", {
-    description: "Adds a new react component",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the component?",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "src/{{kebabCase name}}.tsx",
-        templateFile: "templates/component.hbs",
-      },
-      {
-        type: "append",
-        path: "package.json",
-        pattern: /"exports": {(?<insertion>)/g,
-        template: '"./{{kebabCase name}}": "./src/{{kebabCase name}}.tsx",',
-      },
-    ],
-  });
+
+    plop.setGenerator("component", {
+        description: "Adds a new react component",
+        prompts: [
+            {
+                type: "input",
+                message: "Please enter your component name",
+                name: "name",
+            },
+        ],
+        actions: [
+            {
+                type: "add",
+                templateFile: "templates/component.hbs",
+                path: "src/components/{{kebabCase name}}.tsx",
+            },
+            // {
+            //   type: "add",
+            //   templateFile: "templates/stories.mdx.hbs",
+            //   path: "src/stories/{{pascalCase name}}.stories.mdx",
+            // },
+            {
+                type: "append",
+                path: "src/index.ts",
+                template: 'export { {{pascalCase name}} } from "./components/{{kebabCase name}}";',
+            },
+        ],
+    });
 }
