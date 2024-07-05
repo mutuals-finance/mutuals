d2c8cf03fe82590da9d25998c12243453120cb25
Fabian Piper <fabianpiper@web.de>
2024-07-05 14:42:02 +0200

    fix: space for partners slider

--- apps/landing/src/app/Partners/index.tsx
+++ apps/landing/src/app/Partners/index.tsx
@@ -24,7 +24,7 @@ const networks = [
 
 export default function HomePartners() {
   return (
-    <Box mt={"24"}>
+    <Box mt={"48"}>
       <Container maxW="container.xl" px={{ base: "6", lg: "12" }}>
         <PartnerSlider networks={networks} />
       </Container>
