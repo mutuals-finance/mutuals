610fcd5ab59793c6d25006362ad773c5f98e829d
Fabian Piper <fabianpiper@web.de>
2024-08-21 18:08:13 +0200

    fix: remove log

--- packages/sdk/src/client/mixin.ts
+++ packages/sdk/src/client/mixin.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const applyMixins = (derivedCtor: any, constructors: any[]) => {
   constructors.forEach((baseCtor) => {
-    console.log(baseCtor);
     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
       Object.defineProperty(
         derivedCtor.prototype,
