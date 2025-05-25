03a8ec03c99f255920050e8165930067812dc72b
Fabian Piper <fabianpiper@web.de>
2025-05-25 13:03:40 +0200

    refactor: poolfactory test setup

--- apps/contracts/test/Pool/withdraw.spec.ts
+++ apps/contracts/test/Pool/withdraw.spec.ts
@@ -1,13 +1,8 @@
-import hre from 'hardhat';
-import { Pool, PoolFactory } from '#/types/typechain';
-import { expect } from 'chai';
+import { PoolFactory } from '#/types/typechain';
 import { withSnapshot } from '#/test/utils';
-import Allocation from '@/utils/allocation';
-import { toBigInt, ZeroAddress } from 'ethers';
-import { MerkleTree, PoolLib } from '#/types/typechain/contracts/Pool';
 
-const salt = toBigInt(hre.ethers.randomBytes(16));
-const allocationRoot = hre.ethers.randomBytes(32);
+// const salt = toBigInt(hre.ethers.randomBytes(16));
+// const allocationRoot = hre.ethers.randomBytes(32);
 const recipientNamedSigners = ['recipient0', 'recipient1', 'recipient2'];
 
 const setupTest = withSnapshot(['pool'], async (hre) => {
@@ -18,40 +13,40 @@ const setupTest = withSnapshot(['pool'], async (hre) => {
   const poolFactory = (await hre.ethers.getContract(
     'PoolFactory'
   )) as PoolFactory;
-  const args = [recipient0!.address, allocationRoot, salt] as [
-    string,
-    Uint8Array,
-    bigint,
-  ];
-
-  const createPoolResponse = await poolFactory
-    .connect(recipient0)
-    .createPool(...args);
-  await createPoolResponse.wait();
-
-  const poolAddress = await poolFactory.getAddress(...args);
-
-  const pool = (await hre.ethers.getContractAt(
-    'Pool',
-    poolAddress
-  )) as unknown as Pool;
-
-  const allocations = Allocation.from({
-    [recipient0!.address]: 50,
-    [recipient1!.address]: 50,
-  });
-  const tree = Allocation.buildTree(allocations);
-  const { proof, proofFlags } = tree.getMultiProof([recipientPosition]);
-  const proofParams = {
-    value: proof,
-    flags: proofFlags,
-  } as MerkleTree.MultiProofStruct;
+  // const args = [recipient0!.address, allocationRoot, salt] as [
+  //   string,
+  //   Uint8Array,
+  //   bigint,
+  // ];
+  //
+  // const createPoolResponse = await poolFactory
+  //   .connect(recipient0)
+  //   .createPool(...args);
+  // await createPoolResponse.wait();
+  //
+  // const poolAddress = await poolFactory.getAddress(...args);
+  //
+  // const pool = (await hre.ethers.getContractAt(
+  //   'Pool',
+  //   poolAddress
+  // )) as unknown as Pool;
+  //
+  // const allocations = Allocation.from({
+  //   [recipient0!.address]: 50,
+  //   [recipient1!.address]: 50,
+  // });
+  // const tree = Allocation.buildTree(allocations);
+  // const { proof, proofFlags } = tree.getMultiProof([recipientPosition]);
+  // const proofParams = {
+  //   value: proof,
+  //   flags: proofFlags,
+  // } as MerkleTree.MultiProofStruct;
 
   return {
     poolFactory,
-    pool,
-    proofParams,
-    allocations,
+    // pool,
+    // proofParams,
+    // allocations,
     recipient0,
     recipient1,
     recipient2,
@@ -68,58 +63,58 @@ describe('Pool.withdraw', () => {
     });
 
     it('should successfully execute the withdrawal up to the allotted amount for a recipient', async () => {
-      const { pool, proofParams, allocations, recipient0 } = await setupTest();
-      const request = {
-        allocations: [[allocations[recipientPosition]]],
-        amounts: [0],
-        proof: proofParams,
-      } as PoolLib.WithdrawRequestStruct;
-      expect(
-        pool
-          .connect(recipient0)
-          .withdraw(recipient0!.address, ZeroAddress, request)
-      ).to.not.reverted;
+      // const { pool, proofParams, allocations, recipient0 } = await setupTest();
+      // const request = {
+      //   allocations: [[allocations[recipientPosition]]],
+      //   amounts: [0],
+      //   proof: proofParams,
+      // } as PoolLib.WithdrawRequestStruct;
+      // expect(
+      //   pool
+      //     .connect(recipient0)
+      //     .withdraw(recipient0!.address, ZeroAddress, request)
+      // ).to.not.reverted;
     });
   });
   context('When a user withdraws with invalid parameters', () => {
     it('should revert for a negative amount for a recipient', async () => {
-      const { pool, proofParams, allocations, recipient0 } = await setupTest();
-      const request = {
-        allocations: [[allocations[recipientPosition]]],
-        amounts: [-1],
-        proof: proofParams,
-      } as PoolLib.WithdrawRequestStruct;
-      expect(
-        pool
-          .connect(recipient0)
-          .withdraw(recipient0!.address, ZeroAddress, request)
-      ).to.be.reverted;
+      // const { pool, proofParams, allocations, recipient0 } = await setupTest();
+      // const request = {
+      //   allocations: [[allocations[recipientPosition]]],
+      //   amounts: [-1],
+      //   proof: proofParams,
+      // } as PoolLib.WithdrawRequestStruct;
+      // expect(
+      //   pool
+      //     .connect(recipient0)
+      //     .withdraw(recipient0!.address, ZeroAddress, request)
+      // ).to.be.reverted;
     });
     it('should revert for submitting a wrong proof', async () => {
-      const { pool, proofParams, allocations, recipient0 } = await setupTest();
-      const request = {
-        allocations: [[allocations[wrongRecipientPosition]]],
-        amounts: [0],
-        proof: proofParams,
-      } as PoolLib.WithdrawRequestStruct;
-      expect(
-        pool
-          .connect(recipient0)
-          .withdraw(recipient0!.address, ZeroAddress, request)
-      ).to.be.reverted;
+      // const { pool, proofParams, allocations, recipient0 } = await setupTest();
+      // const request = {
+      //   allocations: [[allocations[wrongRecipientPosition]]],
+      //   amounts: [0],
+      //   proof: proofParams,
+      // } as PoolLib.WithdrawRequestStruct;
+      // expect(
+      //   pool
+      //     .connect(recipient0)
+      //     .withdraw(recipient0!.address, ZeroAddress, request)
+      // ).to.be.reverted;
     });
     it('should revert for a too high amount for a recipient', async () => {
-      const { pool, proofParams, allocations, recipient0 } = await setupTest();
-      const request = {
-        allocations: [[allocations[recipientPosition]]],
-        amounts: [1],
-        proof: proofParams,
-      } as PoolLib.WithdrawRequestStruct;
-      expect(
-        pool
-          .connect(recipient0)
-          .withdraw(recipient0!.address, ZeroAddress, request)
-      ).to.be.reverted;
+      // const { pool, proofParams, allocations, recipient0 } = await setupTest();
+      // const request = {
+      //   allocations: [[allocations[recipientPosition]]],
+      //   amounts: [1],
+      //   proof: proofParams,
+      // } as PoolLib.WithdrawRequestStruct;
+      // expect(
+      //   pool
+      //     .connect(recipient0)
+      //     .withdraw(recipient0!.address, ZeroAddress, request)
+      // ).to.be.reverted;
     });
   });
 });
--- apps/contracts/test/PoolFactory/createPool.spec.ts
+++ apps/contracts/test/PoolFactory/createPool.spec.ts
@@ -1,20 +1,22 @@
-import hre from 'hardhat';
-import { Pool, PoolFactory } from '#/types/typechain';
+import { Pool, PoolFactory, Registry } from '#/types/typechain';
 import { expect } from 'chai';
 import { withSnapshot } from '#/test/utils';
-import { toBigInt } from 'ethers';
 
 const setupTest = withSnapshot(['pool'], async (hre) => {
   const [poolOwnerHonest, poolOwnerMalicious] = await Promise.all([
     hre.ethers.getNamedSigner('poolOwnerHonest'),
     hre.ethers.getNamedSigner('poolOwnerMalicious'),
   ]);
-  const poolFactory = (await hre.ethers.getContract(
+  const poolFactory = (await hre.deployments.get(
     'PoolFactory'
-  )) as PoolFactory;
-  const pool = (await hre.ethers.getContract('Pool')) as Pool;
+  )) as unknown as PoolFactory;
+  const pool = (await hre.deployments.get('Pool')) as unknown as Pool;
+  const registry = (await hre.deployments.get(
+    'Registry'
+  )) as unknown as Registry;
 
   return {
+    registry,
     poolFactory,
     pool,
     poolOwnerHonest,
@@ -22,55 +24,61 @@ const setupTest = withSnapshot(['pool'], async (hre) => {
   };
 });
 
-const salt1 = toBigInt(hre.ethers.randomBytes(16));
-const salt2 = toBigInt(hre.ethers.randomBytes(16));
-const allocationRoot = hre.ethers.randomBytes(32);
+const salt1 = hre.ethers.toBigInt(hre.ethers.randomBytes(16));
+const salt2 = hre.ethers.toBigInt(hre.ethers.randomBytes(16));
 
 describe('PoolFactory.createPool', () => {
   context('When called with valid parameters', () => {
     let createdPool: Pool | null;
 
     before(async () => {
-      const { poolFactory, poolOwnerHonest } = await setupTest();
+      const { registry, poolFactory, poolOwnerHonest } = await setupTest();
+      const params = [
+        poolOwnerHonest.address,
+        registry.target,
+        [],
+        [],
+        salt2,
+      ] as Parameters<PoolFactory['createPool']>;
+
       await poolFactory
         .connect(poolOwnerHonest)
-        .createPool(poolOwnerHonest.address, allocationRoot, salt2)
+        .createPool(...params)
         .then((tx) => tx.wait());
 
-      const create2Address = await poolFactory.getAddress(
-        poolOwnerHonest.address,
-        allocationRoot,
-        salt2
-      );
-      createdPool = await hre.ethers.getContractAt('Pool', create2Address);
+      const create2Address = await poolFactory.getAddress(...params);
+      createdPool = (await hre.ethers.getContractAt(
+        'Pool',
+        create2Address
+      )) as unknown as Pool;
     });
 
     it('should deploy a beacon proxy', async () => {
-      const { poolFactory, poolOwnerHonest } = await setupTest();
+      const { registry, poolFactory, poolOwnerHonest } = await setupTest();
       expect(
         poolFactory
           .connect(poolOwnerHonest)
-          .createPool(poolOwnerHonest.address, allocationRoot, salt1)
+          .createPool(poolOwnerHonest.address, registry.target, [], [], salt1)
       ).to.not.reverted;
     });
 
     it('should emit a valid PoolCreated event', async () => {
-      const { poolFactory, poolOwnerHonest } = await setupTest();
+      const { registry, poolFactory, poolOwnerHonest } = await setupTest();
 
       await expect(
         poolFactory
           .connect(poolOwnerHonest)
-          .createPool(poolOwnerHonest.address, allocationRoot, salt2)
+          .createPool(poolOwnerHonest.address, registry.target, [], [], salt2)
       ).to.emit(poolFactory, 'PoolCreated');
     });
 
     it('should initialize the proxy', async () => {
-      const { poolOwnerMalicious } = await setupTest();
+      const { registry, poolOwnerMalicious } = await setupTest();
 
       await expect(
         createdPool
           ?.connect(poolOwnerMalicious)
-          .__Pool_init(poolOwnerMalicious.address, allocationRoot)
+          .__Pool_init(poolOwnerMalicious.address, registry.target, [], [])
       ).to.be.reverted;
     });
   });
