2266c30bc861cf2450acc86b17f6f733793ddfd5
Fabian Piper <fabianpiper@web.de>
2025-08-15 14:57:31 +0200

    feat: sidebar without motion

--- apps/app/src/features/Shell/Dashboard/Header/index.tsx
+++ apps/app/src/features/Shell/Dashboard/Header/index.tsx
@@ -1,15 +1,32 @@
-import { Form, InputGroup, Stack, MutualsLogo, Input } from "@mutuals/ui";
+"use client";
+
+import {
+  Form,
+  InputGroup,
+  Stack,
+  MutualsLogo,
+  Input,
+  AbsoluteCenter,
+  IconButton,
+  Icon,
+  type UseDisclosureProps,
+} from "@mutuals/ui";
 import { IoSearch } from "react-icons/io5";
 
 import Chain from "./Chain";
 import User from "./User";
+import { VscMenu } from "react-icons/vsc";
 
-export default function ShellDashboardHeader() {
+export type ShellDashboardHeaderProps = UseDisclosureProps;
+
+export default function ShellDashboardHeader({
+  onToggle,
+}: ShellDashboardHeaderProps) {
   return (
     <Stack
       as="header"
       position="sticky"
-      h={{ base: "5rem", md: "3.4rem" }}
+      h={{ base: "4rem", md: "4rem" }}
       px={{ base: "6", lg: "12" }}
       py={"6"}
       zIndex={"50"}
@@ -18,21 +35,36 @@ export default function ShellDashboardHeader() {
       w={"100%"}
       alignItems={"center"}
       justifyContent={"space-between"}
-      gap={{ base: "3", lg: "12" }}
+      gap={{ base: "2", lg: "12" }}
       borderBottomWidth="1px"
       borderColor={"border"}
       bg={"bg"}
       direction={"row"}
     >
-      <Form hideBelow={"lg"}>
-        <InputGroup startElement={<IoSearch />}>
-          <Input size={"sm"} placeholder="Search..." />
-        </InputGroup>
-      </Form>
+      <Stack direction={"row"} gap={4} alignItems={"center"}>
+        <MutualsLogo w={"24"} />
 
-      <MutualsLogo hideFrom={"lg"} w={"24"} mr={"auto"} />
+        <IconButton
+          size={"sm"}
+          aria-label={"Toggle Sidebar"}
+          variant={"ghost"}
+          onClick={onToggle}
+        >
+          <Icon size={"md"}>
+            <VscMenu />
+          </Icon>
+        </IconButton>
+      </Stack>
 
-      <Stack direction={"row"} gap={4} ml={"auto"}>
+      <AbsoluteCenter>
+        <Form hideBelow={"lg"}>
+          <InputGroup startElement={<IoSearch />}>
+            <Input size={"sm"} placeholder="Search..." />
+          </InputGroup>
+        </Form>
+      </AbsoluteCenter>
+
+      <Stack direction={"row"} gap={4}>
         <Chain />
         <User />
       </Stack>
