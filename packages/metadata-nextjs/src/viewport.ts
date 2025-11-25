24444c5ac162e06ecb6c943ae82980490ba1d1a0
Fabian Piper <fabianpiper@web.de>
2025-11-25 16:52:34 +0100

    fix: theme color

--- packages/metadata-nextjs/src/viewport.ts
+++ packages/metadata-nextjs/src/viewport.ts
@@ -2,7 +2,7 @@ import type { Viewport } from "next";
 
 export const defaultViewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#f4f4f5" },
+    { media: "(prefers-color-scheme: light)", color: "#020203" },
     { media: "(prefers-color-scheme: dark)", color: "#020203" },
   ],
 };
