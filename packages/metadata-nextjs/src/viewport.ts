6d880444a1a3a34cb5985cb0cf8c5e4dd76874dc
Fabian Piper <fabianpiper@web.de>
2025-11-25 13:54:40 +0100

    feat: viewport colorscheme

--- packages/metadata-nextjs/src/viewport.ts
+++ packages/metadata-nextjs/src/viewport.ts
@@ -1,7 +1,10 @@
 import type { Viewport } from "next";
 
 export const defaultViewport: Viewport = {
-  themeColor: "#020203",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#f4f4f5" },
+    { media: "(prefers-color-scheme: dark)", color: "#020203" },
+  ],
 };
 
 export function createViewport(props?: Viewport): Viewport {
