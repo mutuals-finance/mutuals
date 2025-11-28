3679bd21720093c30a999ddfebbcb456a743af5e
Fabian Piper <fabianpiper@web.de>
2025-11-28 23:58:40 +0100

    feat: blog main button

--- apps/landing/src/features/Blog/Hero/index.tsx
+++ apps/landing/src/features/Blog/Hero/index.tsx
@@ -57,7 +57,7 @@ export default function BlogHero() {
               </Text>
             </Box>
             <Box>
-              <Button variant={"ghost"}>Read more</Button>
+              <Button variant={"subtle"}>Read more</Button>
             </Box>
           </Stack>
         </GridItem>
